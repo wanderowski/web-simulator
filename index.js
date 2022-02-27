3d53249b7bc47d656452d714d185944210fc25a9
Dauren Beisenkhanov <dauren.beisenkhanov@gmail.com>
2022-02-27 23:19:56 +0600

    add switch for batch mode

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ const startSimBtn = document.querySelector("#startSimBtn");
 const downloadBtn = document.querySelector("#downloadBtn");
 const graphBtn = document.querySelector("#graphBtn");
 const routingSelect = document.querySelector("#routingSelect");
+const batchSwitch = document.querySelector("#batchSwitch");
+const numOfSimsLabel = document.querySelector("#numOfSimsLabel");
 
 graphBtn.addEventListener("click", () => {
   cytoContainer.classList.toggle("hiddenEl");
@@ -64,11 +66,10 @@ graphBtn.addEventListener("click", () => {
 const modalEl = document.querySelector("#modalEl");
 const timeEl = document.querySelector("#timeEl");
 
-let numOfNodes,
-  simTime,
-  routing,
-  stopSim = false;
-
+const handleCheckChange = (event) => {
+  numOfSims.classList.toggle("hiddenEl");
+  numOfSimsLabel.classList.toggle("hiddenEl");
+};
 const convertMetersToPixels = (meters) => meters / 16;
 
 class Message {
@@ -162,6 +163,12 @@ class Node {
 let nodes = [];
 let logArray = [];
 let currentTime = 0;
+let numOfNodes,
+  simTime,
+  routing,
+  batchMode = false,
+  stopSim = false,
+  curSim = 0;
 
 function calculateVelocity(source, destination) {
   const angle = Math.atan2(
@@ -170,8 +177,8 @@ function calculateVelocity(source, destination) {
   );
 
   const velocity = {
-    x: convertMetersToPixels(50) * Math.cos(angle),
-    y: convertMetersToPixels(50) * Math.sin(angle),
+    x: convertMetersToPixels(35) * Math.cos(angle),
+    y: convertMetersToPixels(35) * Math.sin(angle),
   };
 
   return velocity;
@@ -194,8 +201,10 @@ function getRandomPath() {
   ];
   return path;
 }
+let animationId;
 
 function init() {
+  animationId = 0;
   stopSim = false;
   nodes = [];
   currentTime = 0;
@@ -249,127 +258,124 @@ const copyMessageObj = (obj) => {
   return copy;
 };
 
-let animationId;
+let deliveredSim = 0;
+let numOfHops = 0;
 
-function animate() {
-  timeEl.innerHTML = currentTime;
+const animate = async () => {
+  return new Promise((resolve, reject) => {
+    timeEl.innerHTML = currentTime;
 
-  if (currentTime < simTime && !stopSim) {
-    c.fillStyle = "white";
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    if (currentTime < simTime && !stopSim) {
+      c.fillStyle = "white";
+      c.fillRect(0, 0, canvas.width, canvas.height);
 
-    nodes.forEach((node) => {
-      node.update();
+      nodes.forEach((node) => {
+        node.update();
 
-      nodes.forEach((otherNode) => {
-        const dist = Math.hypot(node.x - otherNode.x, node.y - otherNode.y);
-        // Detect the intersection of two coverage areas of nodes
-        if (dist <= node.radius && node.id !== otherNode.id) {
-          // check if the node has been already connected or it's a new connection
-          if (!node.currConnectedNodes.includes(otherNode.id)) {
-            // check the type of routing
-            if (node.message && !otherNode.message && routing == "epidemic") {
-              otherNode.message = copyMessageObj(node.message); // this is done in order to copy the Message and increment id
-              otherNode.message.addHop(
-                `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
-              );
-              console.log(
-                `Successfully transmitted from Node ${node.id} to Node ${otherNode.id} with Epidemic`
-              );
-              if (otherNode.id === otherNode.message?.destination) {
-                console.log("Delivered!");
-                console.log("Hops: ", otherNode.message.hops);
-                stopSim = true;
-              }
-            }
-            if (node.message && !otherNode.message && routing == "snw") {
-              // send the message from source node to the first relay node found
-              if (node.id === 0 && node.message.count === 0) {
-                otherNode.message = copyMessageObj(node.message);
-                otherNode.relaySource = true;
+        nodes.forEach((otherNode) => {
+          const dist = Math.hypot(node.x - otherNode.x, node.y - otherNode.y);
+          // Detect the intersection of two coverage areas of nodes
+          if (dist <= node.radius && node.id !== otherNode.id) {
+            // check if the node has been already connected or it's a new connection
+            if (!node.currConnectedNodes.includes(otherNode.id)) {
+              // check the type of routing
+              if (node.message && !otherNode.message && routing == "epidemic") {
+                otherNode.message = copyMessageObj(node.message); // this is done in order to copy the Message and increment id
                 otherNode.message.addHop(
                   `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
                 );
-                node.message.count = node.message.count + 1;
                 console.log(
-                  `Successfully transmitted from Node ${node.id} to Node ${otherNode.id} with SnW routing`
+                  `Successfully transmitted from Node ${node.id} to Node ${otherNode.id} with Epidemic`
                 );
-              }
-              // checks if it's not the source node, if the count is less than limit, if it's the first relay source, if the
-              // receiving node does not have any message
-              if (
-                node.message.id !== 0 &&
-                node.message?.count < node.message?.limit &&
-                node.relaySource &&
-                !otherNode.message
-              ) {
-                otherNode.message = copyMessageObj(node.message);
-                otherNode.message.addHop(
-                  `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
-                );
+                if (otherNode.id === otherNode.message?.destination) {
+                  console.log("Delivered!");
+                  deliveredSim++;
+                  curSim++;
 
-                console.log(
-                  `(${
-                    otherNode.message.count + 1
-                  }) Successfully transmitted from Node ${node.id} to Node ${
-                    otherNode.id
-                  } with SnW routing`
-                );
-                node.message.count = node.message.count + 1; // increase the coutner for relay node so that we could limit num of messages
+                  console.log("Hops: ", otherNode.message.hops);
+                  stopSim = true;
+                  resolve();
+                }
               }
-              if (otherNode.id === node.message?.destination) {
-                otherNode.message = copyMessageObj(node.message);
-                otherNode.message.addHop(
-                  `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
-                );
-                console.log("Delivered!");
-                console.log("Hops: ", otherNode.message.hops);
-                stopSim = true;
+              if (node.message && !otherNode.message && routing == "snw") {
+                // send the message from source node to the first relay node found
+                if (node.id === 0 && node.message.count === 0) {
+                  otherNode.message = copyMessageObj(node.message);
+                  otherNode.relaySource = true;
+                  otherNode.message.addHop(
+                    `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
+                  );
+                  node.message.count = node.message.count + 1;
+                  console.log(
+                    `Successfully transmitted from Node ${node.id} to Node ${otherNode.id} with SnW routing`
+                  );
+                }
+                // checks if it's not the source node, if the count is less than limit, if it's the first relay source, if the
+                // receiving node does not have any message
+                if (
+                  node.message.id !== 0 &&
+                  node.message?.count < node.message?.limit &&
+                  node.relaySource &&
+                  !otherNode.message
+                ) {
+                  otherNode.message = copyMessageObj(node.message);
+                  otherNode.message.addHop(
+                    `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
+                  );
+
+                  console.log(
+                    `(${
+                      otherNode.message.count + 1
+                    }) Successfully transmitted from Node ${node.id} to Node ${
+                      otherNode.id
+                    } with SnW routing`
+                  );
+                  node.message.count = node.message.count + 1; // increase the coutner for relay node so that we could limit num of messages
+                }
+                if (otherNode.id === node.message?.destination) {
+                  otherNode.message = copyMessageObj(node.message);
+                  otherNode.message.addHop(
+                    `Message ID: ${otherNode.message.id}. Hop: ${node.id} --> ${otherNode.id}`
+                  );
+                  console.log("Delivered!");
+                  console.log("Hops: ", otherNode.message.hops);
+                  deliveredSim++;
+                  stopSim = true;
+                }
               }
+              node.currConnectedNodes.push(otherNode.id);
+              const temp = [...logArray[node.id][otherNode.id]];
+              temp.push(currentTime);
+              logArray[node.id][otherNode.id] = [...temp];
             }
-            // console.log(`Node ${node.id} connected to Node ${otherNode.id}`);
-            node.currConnectedNodes.push(otherNode.id);
+          } else if (
+            dist > node.radius &&
+            node.currConnectedNodes.includes(otherNode.id) &&
+            node.id !== otherNode.id
+          ) {
             const temp = [...logArray[node.id][otherNode.id]];
             temp.push(currentTime);
             logArray[node.id][otherNode.id] = [...temp];
-
-            // console.log(
-            //   `After pushing in Node ${node.id} connected: `,
-            //   logArray
-            // );
+            node.currConnectedNodes = node.currConnectedNodes.filter(
+              (id) => id !== otherNode.id
+            );
           }
-        } else if (
-          dist > node.radius &&
-          node.currConnectedNodes.includes(otherNode.id) &&
-          node.id !== otherNode.id
-        ) {
-          // console.log(`Node ${node.id} disconnected from Node ${otherNode.id}`);
-          const temp = [...logArray[node.id][otherNode.id]];
-          temp.push(currentTime);
-          logArray[node.id][otherNode.id] = [...temp];
-
-          // console.log(
-          //   `After pushing in Node ${node.id} disconnected: `,
-          //   logArray
-          // );
-          node.currConnectedNodes = node.currConnectedNodes.filter(
-            (id) => id !== otherNode.id
-          );
-        }
+        });
       });
-    });
-    animationId = requestAnimationFrame(animate);
-  } else {
-    setTimeout(() => {
-      cancelAnimationFrame(animationId);
+      animationId = requestAnimationFrame(animate);
+    } else {
+      curSim++;
+      resolve();
+
+      // cancelAnimationFrame(animationId);
       startSimBtn.disabled = false;
       routingSelect.disabled = false;
       downloadBtn.disabled = false;
       downloadBtn.onclick = exportToCsv;
-    }, 0);
-  }
-  currentTime++;
-}
+    }
+    currentTime++;
+  });
+};
 
 const drawGraph = () => {};
 
@@ -391,20 +397,28 @@ const exportToCsv = () => {
   drawGraph();
 };
 
-startSimBtn.addEventListener("click", (e) => {
+startSimBtn.addEventListener("click", async (e) => {
   e.preventDefault();
-  numOfNodes = document.querySelector("#numOfNodes").value * 1;
-  simTime = document.querySelector("#simTime").value * 1;
-  routing = routingSelect.value;
-  if (numOfNodes && simTime && routing) {
-    logArray = [...Array(numOfNodes + 2)].map((e) =>
-      Array(numOfNodes + 2).fill(Array(0))
-    );
-    init();
-    startSimBtn.disabled = true;
-    routingSelect.disabled = true;
-    downloadBtn.disabled = true;
 
-    animate();
+  batchMode = batchSwitch.checked;
+  numOfSims = document.querySelector("#numOfSims").value * 1;
+  while (batchMode && curSim < numOfSims) {
+    console.log("hello");
+    numOfNodes = document.querySelector("#numOfNodes").value * 1;
+    simTime = document.querySelector("#simTime").value * 1;
+    routing = routingSelect.value;
+
+    if (numOfNodes && simTime && routing) {
+      console.log("hello x2");
+      logArray = [...Array(numOfNodes + 2)].map((e) =>
+        Array(numOfNodes + 2).fill(Array(0))
+      );
+      init();
+      batchSwitch.disabled = true;
+      startSimBtn.disabled = true;
+      routingSelect.disabled = true;
+      downloadBtn.disabled = true;
+      await animate();
+    }
   }
 });
